4504325bbc1b4105e90aaec180f9e0a000178694
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-13 12:21:19 +0600

    fix:profile translate y

--- src/app/Profile/page.jsx
+++ src/app/Profile/page.jsx
@@ -14,8 +14,8 @@ const ProfilePage = () => {
         />
       </div>
       <div className="max-w-screen-xl px-9 mx-auto bg-[#F6F6F6]">
-        <div className="flex justify-between gap-x-32">
-          <div className="flex w-1/2 h-[200px] items-center justify-between">
+        <div className="flex justify-between gap-x-32 relative">
+          <div className="flex w-9/12 h-[200px] -translate-y-[50%] items-center justify-between">
             <div>
               <Image
                 src="/assets/Profile Pic.png"
@@ -24,7 +24,7 @@ const ProfilePage = () => {
                 alt="Profile Pic"
               />
             </div>
-            <div>
+            <div className="mt-24">
               <h1 className="text-2xl text-[#2B2B2B] font-semibold">
                 John Doe <sup>.</sup>
                 <span className="ml-3 text-[#76A4CE] font-light text-base">
@@ -37,7 +37,7 @@ const ProfilePage = () => {
                 Gamer 👾
               </p>
             </div>
-            <div>
+            <div className="mt-24">
               <button className="text-white bg-[#196AA0] py-2 px-4 rounded-full ml-12 text-sm font-medium">
                 Follow
               </button>
@@ -85,7 +85,7 @@ const ProfilePage = () => {
                 <h3 className="text-2xl text-[#2B2B2B] font-semibold mb-2">
                   Videos
                 </h3>
-                <div>
+                <div className="space-y-4">
                   <div className="relative">
                     <Image
                       src="/assets/video1.jpg"
@@ -386,6 +386,9 @@ const ProfilePage = () => {
                       </svg>
                     </div>
                   </div>
+                  <p className="text-[#196AA0] text-base font-medium text-right mt-2">
+                  More +
+                </p>
                 </div>
               </div>
             </div>
